1ffd5aad1b11e8640b96eae107b3e064afff81a3
AldoBre <aldobresssan@hotmail.com>
2022-11-28 06:39:21 -0300

    Add product CRUD

--- js/listAllUsers.js
+++ js/listAllUsers.js
@@ -142,4 +142,27 @@ async function renderAllUsers(){
 
 }
 
+async function getUserDados () {
+    const token = localStorage.getItem("token")
+    const id = localStorage.getItem("id")
+
+    const allusers = await fetch(`${endpointURL}/user/${id}`,{
+        method: "GET",
+        headers: new Headers({
+            "content-type": "application/json",
+            Authorization: `Bearer ${token}`,
+        })
+    })
+
+    const allUsersJson = await allusers.json()
+
+    console.log(allUsersJson)
+
+
+    const userLogin = document.querySelector("#userLogin").innerHTML = allUsersJson.data.name
+   
+
+}
+
+getUserDados()    
 renderAllUsers()
